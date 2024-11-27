beef19b24ce00128a37b6edd0d695bd23cac21c6
Jeff <jeff999955@gmail.com>
2024-11-26 23:16:34 -0800

    chore: add requirements to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+import os
+
 from setuptools import setup, find_packages
 
-setup(name='multiagent',
-      version='0.0.1',
-      description='Multi-Agent Goal-Driven Communication Environment',
-      url='https://github.com/openai/multiagent-public',
-      author='Igor Mordatch',
-      author_email='mordatch@openai.com',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=['gym', 'numpy-stl']
+# Read requirements.txt
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt")) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="multiagent",
+    version="0.0.1",
+    description="Multi-Agent Goal-Driven Communication Environment",
+    url="https://github.com/openai/multiagent-public",
+    author="Igor Mordatch",
+    author_email="mordatch@openai.com",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements,
 )
